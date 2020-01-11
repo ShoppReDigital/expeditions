905e23c77fd3773746005c0eb4c3486d81140b61
ShoppRe.com <tech.shoppre@gmail.com>
2020-01-11 16:30:59 +0530

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,11 +1,9 @@
 module.exports = {
-  title: 'Vuepress',
-  description: 'This is a Zeit Now 2.0 example',
+  title: 'ShoppRe EXPEDITIONS',
+  description: 'Know more about ShoppRe',
   themeConfig: {
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Guide', link: '/guide/' },
-      { text: 'Config Page', link: '/config' },
     ],
   },
   dest: 'public',
